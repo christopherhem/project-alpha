44a7946caa068da182f3311b11128f6b259f323f
Christopher <chris.hem10@gmail.com>
2022-08-02 16:40:20 -0500

    Feature 15 complete

---
+++ tasks/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from tasks.views import (
+    TaskCreateView,
+)
+
+urlpatterns = [
+    path(
+        "create/",
+        TaskCreateView.as_view(),
+        name="create_task",
+    ),
+]
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
+from tasks.models import Task
+from projects.models import Project
 
 # Create your views here.
+class TaskCreateView(LoginRequiredMixin, CreateView):
+    model = Task
+    template_name = "tasks/create.html"
+    fields = ["name", "start_date", "due_date", "project", "assignee"]
+
+    def get_success_url(self):
+        return reverse_lazy("show_project", args=[self.object.project.pk])
