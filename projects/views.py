52d87b158944a07c3d70db4b263ca46f8d71736f
Christopher <chris.hem10@gmail.com>
2022-08-01 23:19:23 -0500

    Feature 14 complete

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 
 from projects.models import Project
 
@@ -17,3 +18,15 @@ class ProjectListView(LoginRequiredMixin, ListView):
 class ProjectDetailView(LoginRequiredMixin, DetailView):
     model = Project
     template_name = "projects/detail.html"
+
+
+class ProjectCreateView(LoginRequiredMixin, CreateView):
+    model = Project
+    template_name = "projects/create.html"
+    fields = ["name", "description", "members"]
+
+    def form_valid(self, form):
+        project = form.save(commit=False)
+        project.save()
+        form.save_m2m()
+        return redirect("show_project", pk=project.pk)
